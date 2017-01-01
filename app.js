4d4b18aab12b031c2a248c0253cc36e8e748f34e
Tomas Ruud <tomasruu@gmail.com>
2017-01-01 22:15:54 +0100

    Testing new way to get tracks

--- app.js
+++ app.js
@@ -1,53 +1,61 @@
-var redirect = 'http://downloader.soundcloud.ruud.ninja/callback.html';
-var token    = null;
-var limit    = 100;
+var token = null;
+
+var config = {
+  redirect:  'http://downloader.soundcloud.ruud.ninja/callback.html',
+  client_id: 'c205c3e2eedb509dff1c1147765b055d',
+  limit:     10
+};
 
 $('#authorize').on('click', function() {
   SC.initialize({
-    client_id:    'c205c3e2eedb509dff1c1147765b055d',
-    redirect_uri: redirect
+    client_id:    config.client_id,
+    redirect_uri: config.redirect
   });
 
   SC.connect()
       .then(function(session) {
         token = session.oauth_token;
 
-        $('[data-hide="after-auth"]').hide();
-        $('[data-show="after-auth"]').show();
+        animate_step('after-auth');
 
-        return SC.get('/me')
+        return SC.get('/me/tracks', {limit: config.limit, linked_partitioning: 1})
       })
-      .then(function(data) {
-        var track_count = (data.private_tracks_count || 0) + (data.track_count || 0);
-        var pages       = Math.floor(track_count / limit);
+      .then(function(tracks) {
+        console.dir(tracks);
+        var next_page = tracks['next_href'];
+        append(tracks.collection);
 
-        for(var page = 0; page <= pages; page++) {
-          fetch_and_append(limit, page);
+        while(next_page) {
+          SC.get(next_page).then(function(tracks) {
+            console.dir(tracks);
+
+            next_page = tracks['next_href'];
+            append(tracks.collection);
+          });
         }
       });
 });
 
-function fetch_and_append(limit, page) {
-  SC.get('/me/tracks', {limit: limit, linked_partitioning: page})
-      .then(function(data) {
-        data = data.collection;
+function append(tracks) {
+  var track_list = $('#tracks');
 
-        var track_list = $('#tracks');
+  animate_step('after-fetch');
 
-        $('[data-hide="after-fetch"]').hide();
-        $('[data-show="after-fetch"]').show();
+  for(var index in tracks) {
+    var track = tracks[index];
 
-        for(var index in data) {
-          var track = data[index];
+    var download_url = track.download_url;
+    var title        = track.title;
 
-          var download_url = track.download_url;
-          var title        = track.title;
+    var append = '<li>';
+    append += '<a href="' + download_url + '?oauth_token=' + token + '">' + title + '</a>';
+    append += '</li>';
 
-          var append = '<li>';
-          append += '<a href="' + download_url + '?oauth_token=' + token + '">' + title + '</a>';
-          append += '</li>';
+    track_list.append(append);
+  }
+}
 
-          track_list.append(append);
-        }
-      });
+function animate_step(step) {
+  $('[data-hide="' + step + '"]').hide();
+  $('[data-show="' + step + '"]').show();
 }
