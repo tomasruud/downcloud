3630cbd38ed810b4bf3d4d4a20b5c667399cf0a6
Tomas Ruud <tomasruud@users.noreply.github.com>
2018-01-30 15:31:20 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var token = null;
 
 var config = {
-  redirect:  'http://downcloud.ruud.ninja/callback.html',
+  redirect:  'https://downcloud.ruud.ninja/callback.html',
   client_id: 'c205c3e2eedb509dff1c1147765b055d',
   limit:     200
 };
