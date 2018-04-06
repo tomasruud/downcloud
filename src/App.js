82d70649d8221b628ce4bbed4841bbb2a9665aeb
Tomas Ruud <tomas@tomasruud.com>
2018-04-06 09:41:10 +0200

    :fire: Getting the basics

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <header>
-          <img src={logo} alt='logo' />
-          <h1>Welcome to React</h1>
-        </header>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>Greetings</div>
     )
   }
 }
